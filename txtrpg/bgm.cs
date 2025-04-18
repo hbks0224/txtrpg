960c07c7f6de665cb649386e5073afc5d23faac2
hbks0224 <hbks1234@gmail.com>
2025-04-18 11:03:08 +0900

    bgm 재생 방법 변경 bin 폴더에 있는 mp3파일 재생 => Git에 업로드된 mp3파일의 URL주소로 재생

--- txtrpg/bgm.cs
+++ txtrpg/bgm.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Threading;
 
 using WMPLib;
+using static System.Net.WebRequestMethods;
 
 
 namespace txtrpg
@@ -18,14 +19,14 @@ namespace txtrpg
         {
             ThreadPool.QueueUserWorkItem(_ =>
             {
-                String root = AppDomain.CurrentDomain.BaseDirectory;
+                //String root = AppDomain.CurrentDomain.BaseDirectory;
 
-                String result = Path.Combine(root, "bgm.mp3");
+                // String result = Path.Combine(root, "bgm.mp3");
 
                 // Console.WriteLine(result); 
                 WindowsMediaPlayer player = new WindowsMediaPlayer();
                 player.settings.setMode("loop", true); // 반복 재생 설정
-                player.URL = result;  //
+                player.URL = @"https://hbks0224.github.io/txtrpg/bgm.mp3";  //git에 업로드된 bgm 재생
                 //Console.WriteLine(root);
 
                 player.controls.play(); // 재생 시작
